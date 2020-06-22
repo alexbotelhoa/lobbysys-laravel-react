13964db77b940b17f899695acf651f93c646693f
Alex Botelho <alexbotelho1@hotmail.com>
2020-06-22 10:51:28 -0300

    Backend 95% Frontend 20%

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
+//use Illuminate\Support\Facades\Auth;
+//use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,56 +18,48 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Auth::routes();
+//Auth::routes();
 
-Route::get('/dashboard', 'DashboardController@index')
-    ->name('dashboard');
+//Route::get('/dashboard', '--DashboardController@index')
+//    ->name('dashboard');
 
 
 /*
  * Rotas das páginas de Administração de Visitantes - Tabela Visitors
  */
-Route::prefix('/visitors')->group(function () {
-
-    Route::get('/', 'VisitorController@index')
-        ->name('visitors.index');
-
-    Route::get('/new', 'VisitorController@create')
-        ->name('visitors.create');
-
-    Route::get('/{id}/edit', 'VisitorController@edit')
-        ->name('visitors.edit');
-
-});
+//Route::prefix('/visitors')->group(function () {
+//
+//    Route::get('/', 'VisitorController@index')
+//        ->name('visitors.index');
+//
+//    Route::get('/new', 'VisitorController@create')
+//        ->name('visitors.create');
+//
+//    Route::get('/{id}/edit', 'VisitorController@edit')
+//        ->name('visitors.edit');
+//
+//});
 
 
 /*
  * Rotas das páginas de Administração de Salas - Tabela Rooms
  */
-Route::prefix('/rooms')->group(function () {
-
-    Route::get('/', 'RoomController@index')
-        ->name('rooms.index');
-
-    Route::get('/new', 'RoomController@create')
-        ->name('rooms.create');
-
-    Route::get('/{id}/edit', 'RoomController@edit')
-        ->name('rooms.edit');
-
-});
+//Route::prefix('/rooms')->group(function () {
+//
+//    Route::get('/', 'RoomController@index')
+//        ->name('rooms.index');
+//
+//    Route::get('/new', 'RoomController@create')
+//        ->name('rooms.create');
+//
+//    Route::get('/{id}/edit', 'RoomController@edit')
+//        ->name('rooms.edit');
+//
+//});
 
 
 /*
  * Rota da visualização dos Registros da Portaria - Tabela Concierge
  */
-Route::get('/concierges', 'ConciergeController@index')
-    ->name('concierges.index');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+//Route::get('/concierges', 'ConciergeController@index')
+//    ->name('concierges.index');
