39cd3ea6dd0902e47a53cb0bd6d1c1540a2acf85
Alex Botelho <alexbotelho1@hotmail.com>
2020-07-09 18:11:08 -0300

    Correção git.ignore

---
+++ backend/tests/Feature/MiddlewaresTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Room;
+use Tests\TestCase;
+
+class MiddlewaresTest extends TestCase
+{
+    /**
+     * Deve ser VÁLIDO quando
+     * a ROTA GET (Index) for CARREGADA
+     * e retornar o STATUS '200'
+     *
+     * @test
+     */
+    public function shouldBeValidWhenMiddlewareHasLoaded()
+    {
+
+    }
+}
